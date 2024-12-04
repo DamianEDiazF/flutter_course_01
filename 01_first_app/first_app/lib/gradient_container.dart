5adfbd4879228b02eba2bcfd128601a537b7284d
DamianEDiazF <87387761+DamianEDiazF@users.noreply.github.com>
2024-12-04 17:15:48 +0100

    Up to lesson 44

---
+++ 01_first_app/first_app/lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({
+    super.key,
+    this.colors = const [Colors.deepPurple, Colors.black],
+  });
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText("Hello Pepperoni!"),
+      ),
+    );
+  }
+}
--- 01_first_app/first_app/lib/main.dart
+++ 01_first_app/first_app/lib/main.dart
@@ -1,26 +1,12 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.black, Colors.white],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28.0,
-            ),
-          ),
-        ),
+      body: GradientContainer(
+        colors: [Colors.deepOrange, Colors.lime],
       ),
     ),
   ));
