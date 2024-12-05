3f37baf949a165dec59833aec03a2e74f653e24c
Damian Diaz <damianediazf@gmail.com>
2024-12-05 22:06:09 +0100

    Finished first section

--- 01_first_app/first_app/lib/dice_roller.dart
+++ 01_first_app/first_app/lib/dice_roller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random();
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -11,14 +13,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-5.png';
+  var currentDiceRoll = 5;
 
   void rollDice() {
-    var nextPizza = Random().nextInt(6) + 1;
-    print("Pizza: $nextPizza");
-
     setState(() {
-      activeDiceImage = 'assets/images/dice-$nextPizza.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -28,7 +27,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(
--- 01_first_app/first_app/lib/main.dart
+++ 01_first_app/first_app/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.deepPurple,
       body: GradientContainer(
-        colors: const [Colors.deepOrange, Colors.lime],
+        colors: [Colors.deepOrange, Colors.lime],
       ),
     ),
   ));
